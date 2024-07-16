d379e6f9e532b4f8da01dd3f81101b361f0ff6e1
BaljeetSharma98 <baljeetsharma.abc@gmail.com>
2024-07-17 00:57:59 +0530

    Update Assessment.sol

--- Assessment.sol
+++ Assessment.sol
@@ -2,31 +2,31 @@
 pragma solidity 0.8.26;
 
 contract MyHome {
-    uint private Salary;
-    uint private Fees;
-    uint private Expenses;
+    uint private FatherSalary;
+    uint private SchoolFees;
+    uint private HomeExpenses;
     uint private PocketMoney;
 
-    function setExpenses(uint _Salary, uint _Fees,uint _Expenses) public {
-         Salary = _Salary; 
-         Fees = _Fees;
-         Expenses = _Expenses;
+    function setExpenses(uint _fatherSalary, uint _SchoolFees,uint _HomeExpenses) public {
+         FatherSalary = _fatherSalary; 
+         SchoolFees = _SchoolFees;
+         HomeExpenses = _HomeExpenses;
 
-         if(Salary<Fees + Expenses){
-            revert("Salary must be greater than expenses"); // Here revert function stop person to Pay Expenses if greater than his salary.
+         if(FatherSalary<SchoolFees + HomeExpenses){
+            revert("Salary must be greater than expenses"); // Here revert function stop father to Pay Expenses if greater than his salary.
          }
     }
     
     function Children_PocketMoney(uint _PocketMoney) public {
-        require(PocketMoney <= 100, "Children Should have to spent less");  // Require ensures that children pocketmoney is less than 100 dollars.
+        require(PocketMoney < 100, "Children Should have to spent less");  // Require ensures that children pocketmoney is less than 100 dollars.
         PocketMoney  = _PocketMoney;
         
     }
 
     function Salary_Remaining() public view returns (uint) {
-        uint remainingSalary = Salary - Fees - Expenses - PocketMoney;
+        uint remainingSalary = FatherSalary - SchoolFees - HomeExpenses - PocketMoney;
 
-        assert(remainingSalary < Salary); // After the expenses, remaining salary must be less than the initial Salary.
+        assert(remainingSalary < FatherSalary); // After the expenses, remaining salary must be less than the initial Salary.
         return remainingSalary;
     }
 }
